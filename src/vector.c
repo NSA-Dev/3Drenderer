edbf5904e98003417318fc4f17b9ed32f817c78d
NSA_Dev <nixem256@gmail.com>
2025-03-09 17:07:38 +0100

    implemented vector rotations (x,y,z)

--- src/vector.c
+++ src/vector.c
@@ -1,6 +1,38 @@
+#include <math.h>
 #include "vector.h"
-#include <stdio.h>
+
 
 vec2_t project (vec3_t* point) {
         return (vec2_t) {.x = point->x / point->z , .y = point->y / point->z}; 
+}
+
+void vec3_rotate_x(vec3_t* v, float angle) {
+   float r_cos = cos(angle);
+   float r_sin = sin(angle);
+    
+   float r_y = (v->y * r_cos) - (v->z * r_sin);
+   float r_z = (v->z * r_cos) + (v->y * r_sin);
+   
+   v->y = r_y;
+   v->z = r_z; 
+}
+void vec3_rotate_y(vec3_t* v, float angle) {
+   float r_cos = cos(angle);
+   float r_sin = sin(angle);
+    
+   float r_x = (v->x * r_cos) - (v->z * r_sin);
+   float r_z = (v->z * r_cos) + (v->x * r_sin);
+   
+   v->x = r_x;
+   v->z = r_z;
+}
+void vec3_rotate_z(vec3_t* v, float angle) {
+   float r_cos = cos(angle);
+   float r_sin = sin(angle);
+
+   float r_x = (v->x * r_cos) - (v->y * r_sin);
+   float r_y = (v->y * r_cos) + (v->x * r_sin);
+
+   v->x = r_x;
+   v->y = r_y;
 }
