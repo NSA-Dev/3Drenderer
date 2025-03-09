9d97aa4375f94dd7caa3bd4a1380009329ab1eaf
NSA_Dev <nixem256@gmail.com>
2025-03-09 17:53:50 +0100

    fixed sign issue

--- src/vector.c
+++ src/vector.c
@@ -20,8 +20,8 @@ void vec3_rotate_y(vec3_t* v, float angle) {
    float r_cos = cos(angle);
    float r_sin = sin(angle);
     
-   float r_x = (v->x * r_cos) - (v->z * r_sin);
-   float r_z = (v->z * r_cos) + (v->x * r_sin);
+   float r_x = (v->x * r_cos) + (v->z * r_sin);
+   float r_z = (v->z * r_cos) - (v->x * r_sin);
    
    v->x = r_x;
    v->z = r_z;
