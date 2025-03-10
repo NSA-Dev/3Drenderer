75949fbb9505cf404e6a52c56213fa35f072b7b9
NSA_Dev <nixem256@gmail.com>
2025-03-11 00:08:53 +0100

    added triangle_t

--- src/triangle.h
+++ src/triangle.h
@@ -1,11 +1,19 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+#include "vector.h"
+
+// used for storing vertex indices
 typedef struct {
     int a;
     int b;
     int c;
 } face_t; 
+
+// used for storing actual (x,y) coordinates for each vert
+typedef struct {
+    vec2_t points[3];
+} triangle_t; 
 
 
 #endif
