48f4b217ca27094680a886a3ae3f4a29f984a58c
NSA_Dev <nixem256@gmail.com>
2025-03-11 22:50:18 +0100

    added draw_triangle()

--- src/display.c
+++ src/display.c
@@ -109,6 +109,12 @@ void draw_rect(int x, int y, int w, int h, uint32_t color) {
     }
 }
 
+void draw_triangle(int x1, int y1, int x2, int y2, int x3, int y3, uint32_t color) {
+    draw_bresLine(x1, y1, x2, y2, color);
+    draw_bresLine(x2, y2, x3, y3, color);
+    draw_bresLine(x3, y3, x1, y1, color);
+} 
+
 void render_framebuffer(void) {
     SDL_UpdateTexture(
         framebuffer_texture,
--- src/display.h
+++ src/display.h
@@ -20,7 +20,8 @@ bool init_win(void);
 void draw_grid(int spacing, uint32_t color);
 void draw_pixel(int x, int y, uint32_t color);
 void draw_bresLine(int x1, int y1, int x2, int y2, uint32_t color); 
-void draw_rect(int x, int y, int w, int h, uint32_t color); 
+void draw_rect(int x, int y, int w, int h, uint32_t color);
+void draw_triangle(int x1, int y1, int x2, int y2, int x3, int y3, uint32_t color); // uses Bresenheim's
 void render_framebuffer(void);
 void clear_framebuffer(uint32_t color);
 void free_resources(void);
