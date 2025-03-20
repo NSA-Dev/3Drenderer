25ea4484eec5aacd15f71a0a227d6c3565105d93
NSA_Dev <nixem256@gmail.com>
2025-03-20 20:10:14 +0100

    implemented mat4_mult_vec4() operation

--- src/matrix.h
+++ src/matrix.h
@@ -1,6 +1,7 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include "vector.h"
 
 typedef struct {
     float m[4][4];
@@ -9,4 +10,5 @@ typedef struct {
 
 mat4_t mat4_make_identity(void);
 mat4_t mat4_make_scale(float sx, float sy, float sz);
+vec4_t mat4_mult_vec4(mat4_t* m, vec4_t* v);
 #endif
