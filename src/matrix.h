ed633ae4119e2cbea37eba03135e4b3cfbe601b6
NSA_Dev <nixem256@gmail.com>
2025-04-09 23:24:56 +0200

    minor changes

--- src/matrix.h
+++ src/matrix.h
@@ -8,14 +8,15 @@ typedef struct {
     float m[4][4];
 } mat4_t;
 
-
+/* mat4_t constructors */
 mat4_t mat4_make_identity(void);
+mat4_t mat4_make_perspective(float fov, float aspect, float znear, float zfar);
 mat4_t mat4_make_scale(float sx, float sy, float sz);
 mat4_t mat4_make_translation(float tx, float ty, float tz);
 mat4_t mat4_make_rotation_x(float angle);
 mat4_t mat4_make_rotation_y(float angle);
 mat4_t mat4_make_rotation_z(float angle);
+/* operations*/
 vec4_t mat4_mult_vec4(mat4_t* m, vec4_t* v);
 mat4_t mat4_mult_mat4(mat4_t* a, mat4_t* b);
-
 #endif
