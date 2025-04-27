a64de78baa2a77cd8a6e566cd34a940d5006dbe2
NSA_Dev <nixem256@gmail.com>
2025-04-27 19:41:07 +0200

    started work on texture.c

---
+++ src/texture.h
@@ -0,0 +1,14 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+#include <stdint.h>
+typedef struct {
+    float u;
+    float v;
+} text2_t;
+
+extern int texture_width;
+extern int texture_height; 
+
+extern const uint8_t REDBRICK_TEXTURE[]; 
+
+#endif 
